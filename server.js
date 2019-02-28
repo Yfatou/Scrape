406195c8b725339e875dfa8b1f0b72a522464664
Yaye Fatou Thiam <yayefatouthiam@Yayes-MacBook-Air.local>
2019-02-27 20:21:59 -0600

    after installs

---
+++ server.js
@@ -0,0 +1,36 @@
+var express = require("express");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+
+var PORT = 3000;
+
+// Initialize Express
+var app = express();
+
+var routes = require("./controllers/routes");
+app.use(routes);
+
+// Configure middleware
+// Use morgan logger for logging requests
+app.use(logger("dev"));
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Make public a static folder
+app.use(express.static("public"));
+
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.use(routes);
+
+
+// Connect to the Mongo DB
+mongoose.connect("mongodb://localhost/mongoheadline", { useNewUrlParser: true });
+
+// Start the server
+app.listen(PORT, function() {
+  console.log("App running on port " + PORT + "!");
+});
